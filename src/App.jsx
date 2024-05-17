7f03f184d69277e714ea739d4858e2ef9d647c88
Olha Krivtsova <ol_right@ukr.net>
2024-05-17 15:43:57 +0300

    Add </HashRouter> for hosting app in Github

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        {/* <Route path='*' element={<NotFound />} /> */}
+        <Route path='*' element={<NotFound />} />
         <Route index element={<Home />} />
         <Route path='email' element={<Email />} />
       </Route>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import {UserContextProvider} from './store/auth-context';
@@ -8,11 +8,13 @@ import {EmailContextProvider} from './store/email-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
+    {/* <BrowserRouter>  */}
     <UserContextProvider>
       <EmailContextProvider>
         <App />
       </EmailContextProvider>
     </UserContextProvider>
-  </BrowserRouter>
+    {/* </BrowserRouter> */}
+  </HashRouter>
 );
